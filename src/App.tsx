601a3ba97b6c88c1e985e31518b585717c4009f2
Dmitry <dsherbakov@slb.com>
2024-11-20 09:27:11 +0500

    etc

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ function App() {
     <><div>
         <span>English</span>
         <ul>
-            <li><a href={'/english/ps'}>Present simple</a> </li>
-            <li><a href={'/english/tobe/'}>To be</a></li>
-            <li><a href={'/english/pv'}>Phrases ver</a>b</li>
-            <li><a href={'/english/dp'}>Common phrases</a> </li>
-            <li><a href={'/english/table'}>Table of verbs</a> </li>
+            <li><a href={'/school-education/english/ps'}>Present simple</a> </li>
+            <li><a href={'/school-education/english/tobe/'}>To be</a></li>
+            <li><a href={'/school-education/english/pv'}>Phrases ver</a>b</li>
+            <li><a href={'/school-education/english/dp'}>Common phrases</a> </li>
+            <li><a href={'/school-education/english/table'}>Table of verbs</a> </li>
         </ul>
     </div>
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -10,17 +10,17 @@ import {DailyPhrases} from "./components/english/Common/Daily.tsx";
 import {TableOfVerbs} from "./components/english/TableOfVerbs/TableOfVerbs.tsx";
 
 const router = createBrowserRouter([{
-    path: "/", element: <App/>
+    path: "/school-education", element: <App/>
 }, {
-    path: '/english/ps', element: <EnglishPresentSimple/>
+    path: '/school-education/english/ps', element: <EnglishPresentSimple/>
 }, {
-    path: '/english/tobe', element: <EnglishToBe/>
+    path: '/school-education/english/tobe', element: <EnglishToBe/>
 }, {
-    path: '/english/pv', element: <Phrases/>
+    path: '/school-education/english/pv', element: <Phrases/>
 }, {
-    path: '/english/dp', element: <DailyPhrases/>
+    path: '/school-education/english/dp', element: <DailyPhrases/>
 }, {
-    path: '/english/table', element: <TableOfVerbs/>
+    path: '/school-education/english/table', element: <TableOfVerbs/>
 }])
 
 createRoot(document.getElementById('root')!).render(<StrictMode>
