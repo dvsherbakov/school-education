e32778dc6ebc71f842c967c276036894d907f9d4
Dmitry <dsherbakov@slb.com>
2024-11-20 17:02:05 +0500

    etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
+import './App.css'
+import {useState} from "react";
+import {EnglishPresentSimple} from "./components/english/PresentSimple/EnglishPresentSimple.tsx";
+import {EnglishToBe} from "./components/english/ToBe/EnglishToBe.tsx";
+import {DailyPhrases} from "./components/english/Common/Daily.tsx";
+import {Phrases} from "./components/english/Phrases/Phrases.tsx";
+import {TableOfVerbs} from "./components/english/TableOfVerbs/TableOfVerbs.tsx";
+
+const params = new Map([
+    [0, <EnglishPresentSimple/>],
+    [1, <EnglishToBe/>],
+    [2, <Phrases/>],
+    [3, <DailyPhrases/>],
+    [4, <TableOfVerbs/>],
+])
 
 function App() {
-  return (
-    <><div>
-        <span>English</span>
-        <ul>
-            <li><a href={'/school-education/english/ps'}>Present simple</a> </li>
-            <li><a href={'/school-education/english/tobe/'}>To be</a></li>
-            <li><a href={'/school-education/english/pv'}>Phrases ver</a>b</li>
-            <li><a href={'/school-education/english/dp'}>Common phrases</a> </li>
-            <li><a href={'/school-education/english/table'}>Table of verbs</a> </li>
-        </ul>
-    </div>
-    </>
-  );
+    const [selected, setSelected] = useState<number>(0)
+    return (
+        <>
+            <div className={'nav'}>
+                <ul>
+                    <li><span onClick={() => setSelected(0)}>Present simple</span></li>
+                    <li><span onClick={() => setSelected(1)}>To be</span></li>
+                    <li><span onClick={() => setSelected(2)}>Phrases verb</span></li>
+                    <li><span onClick={() => setSelected(3)}>Common phrases</span></li>
+                    <li><span onClick={() => setSelected(4)}>Table of verbs</span></li>
+                </ul>
+
+            </div>
+            <div>{params.get(selected)}</div>
+        </>
+    );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,27 +2,7 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import {EnglishPresentSimple} from "./components/english/PresentSimple/EnglishPresentSimple.tsx";
-import {EnglishToBe} from "./components/english/ToBe/EnglishToBe.tsx";
-import {Phrases} from "./components/english/Phrases/Phrases.tsx";
-import {DailyPhrases} from "./components/english/Common/Daily.tsx";
-import {TableOfVerbs} from "./components/english/TableOfVerbs/TableOfVerbs.tsx";
-
-const router = createBrowserRouter([{
-    path: "/school-education", element: <App/>
-}, {
-    path: '/school-education/english/ps', element: <EnglishPresentSimple/>
-}, {
-    path: '/school-education/english/tobe', element: <EnglishToBe/>
-}, {
-    path: '/school-education/english/pv', element: <Phrases/>
-}, {
-    path: '/school-education/english/dp', element: <DailyPhrases/>
-}, {
-    path: '/school-education/english/table', element: <TableOfVerbs/>
-}])
 
 createRoot(document.getElementById('root')!).render(<StrictMode>
-    <RouterProvider router={router}/>
+<App/>
 </StrictMode>,)
